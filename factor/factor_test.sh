ce53e61ca0a540a3f1ec508217ee16fc6a12603f
Peng Gu <penggu@gmail.com>
2018-02-23 16:37:33 -0800

    Allow inputs for test script

--- factor/factor_test.sh
+++ factor/factor_test.sh
@@ -1,2 +1,8 @@
 go build
-for i in `seq 2 10`; do ./factor $i; done
+
+start=2
+stop=10
+if [[ ! -z $1 ]]; then start=$1; fi
+if [[ ! -z $2 ]]; then stop=$2; fi
+
+for i in `seq $start $stop`; do ./factor $i; done
